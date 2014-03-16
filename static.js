2879126cf7e3b380abbeddbdf9163e78ec961653
alexjab <alexandre.jablon@gmail.com>
2014-03-16 15:42:16 +0100

    improve events display and events managements in the pseudo js file

--- static.js
+++ static.js
@@ -10,30 +10,34 @@ var data_path = process.env.NODE_ENV !== 'test'?'./js_data/':'./test/data/';
 
 var conf = require (conf_path+'conf.js');
 
-var _jquery_data = fs.readFileSync (data_path+'jquery.min.js');
-var _socketio_data = fs.readFileSync (data_path+'socket.io.min.js');
+var jquery_data = fs.readFileSync (data_path+'jquery.min.js');
+var socketio_data = fs.readFileSync (data_path+'socket.io.min.js');
 
 var get_js_file = exports.get_js_file = function (config, _req) {
-  var _head = "/*\n * made with ghostmill.js\n */\n";
-  var _socket = ""; 
+  var head = "/*\n * made with ghostmill.js\n */\n";
+  var socket = "";
   var selectors = _.filter (config.sites.selectors, function (sel) {return sel.page === _req.url;});
   if (selectors.length > 0) {
-    _socket = "\nvar ghostmill_socket = io.connect('"+conf.server.base_url+"');";
     if (!config.static.use_socketio) {
-      _socketio_data = "";
+      socketio_data = "";
     }
     if (!config.static.use_jquery) {
-      _jquery_data = "";
+      jquery_data = "";
     }
-  } else {
-    _socketio_data = "";
-    _jquery_data = "";
-  }
-  var _listeners = (selectors).reduce (function (memo, sel) {
-    return memo + 
-      "$('"+sel.sel+"').on('"+sel.event+"',function(){console.log('event');ghostmill_socket.emit('event',{'type':'tick','ip': '"+_req.ip+"', 'url': '"+_req.url+"', 'sel':'"+sel.sel+"','event':'"+sel.event+"'})});";
-  }, "");
 
-  return _head + _jquery_data + _socketio_data + _socket + _listeners;
+    var gm_object = "var ghostmill={socket:io.connect('"+conf.server.base_url+"'),send:function(sel){if("+config.static.debug_mode+"===true)console.log('send');this.socket.emit('event',{'type':'tick','ip': '"+_req.ip+"', 'url': '"+_req.url+"', 'sel':sel.sel,'event':sel.e});}};";
+    var listeners = (selectors).reduce (function (memo, sel) {
+      return memo +
+        "$('"+sel.sel+"').on('"+sel.event+"',function(){ghostmill.send({sel:'"+sel.sel+"', e:'"+sel.event+"'});});";
+    }, "");
+
+    return head + jquery_data + socketio_data + gm_object + listeners;
+  } else {
+    socketio_data = "";
+    jquery_data = "";
+
+    return head;
+  }
+
 };
 
